3b3790480ef7e9958cf8fa77cdc902b0cf0e656b
Stas <oleksii.stanislav@student.uzhnu.edu.ua>
2026-02-26 00:13:02 +0200

    feat(firebase): add firebase config

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-2359694339-e5d99",
-  "appId": "1:181940963698:web:6015a701b5554208bbf5eb",
-  "apiKey": "AIzaSyAr4W1_Q0ZV2Cs3Vh2kMPqa9x28ESFcelc",
-  "authDomain": "studio-2359694339-e5d99.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "181940963698"
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  measurementId: "",
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
 };
